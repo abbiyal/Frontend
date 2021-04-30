6bcfcde006d464a7a1a523eceb0cce3575d5e3b5
pnkj-jknp <pnkj.chhbra123@gmail.com>
2021-04-30 18:08:11 +0530

    Added enter location manually

---
+++ app/src/main/java/medmart/loginmedmart/MapActivity/PlacesSearch.java
@@ -0,0 +1,14 @@
+package medmart.loginmedmart.MapActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlacesSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_search);
+    }
+}
