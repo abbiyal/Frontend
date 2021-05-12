34234f9a8f84dfe38fe7d3b63b4a2df60f7e4f3c
unknown <abhibiyal@gmail.com>
2021-05-12 15:35:07 +0530

    Profile And SHopInventory Integrated

--- app/src/main/java/medmart/loginmedmart/UtilityClasses/RetrofitInterface.java
+++ app/src/main/java/medmart/loginmedmart/UtilityClasses/RetrofitInterface.java
@@ -19,38 +19,45 @@ public interface RetrofitInterface {
     Call<Jwt> getAccessToken(@Body LoginCredentials login);
 
     @POST("/api/signup")
-    Call<HashMap<String,String>> addUser(@Body HashMap<String, String> user);
+    Call<HashMap<String, String>> addUser(@Body HashMap<String, String> user);
 
     @POST("/api/forgot/sendtoken")
-    Call<HashMap<String,String>> sendToken(@Body HashMap<String,String> email);
+    Call<HashMap<String, String>> sendToken(@Body HashMap<String, String> email);
 
     @POST("/api/forgot/verifytoken")
-    Call<HashMap<String,String>> verifyOtp(@Body HashMap<String,String> otp);
+    Call<HashMap<String, String>> verifyOtp(@Body HashMap<String, String> otp);
 
     @POST("/api/forgot/updatepassword")
-    Call<HashMap<String,String>> updatePassword(@Body HashMap<String,String> password);
+    Call<HashMap<String, String>> updatePassword(@Body HashMap<String, String> password);
 
     @GET("/med-inventory/search")
-    Call<List<ProductCatalogue>> getSearchResults(@Header("Authorization") String token, @QueryMap Map<String,String> params);
+    Call<List<ProductCatalogue>> getSearchResults(@Header("Authorization") String token, @QueryMap Map<String, String> params);
 
-    @GET("/api/updateName")
-    Call<HashMap<String,String>> updateName(@Header("Authorization") String token, @QueryMap Map<String,String> params);
+    @POST("/api/profile/updateName")
+    Call<HashMap<String, String>> updateName(@Header("Authorization") String token, @Body Map<String, String> params);
 
-    @GET("/api/updatePhone")
-    Call<HashMap<String,String>> updatePhone(@Header("Authorization") String token, @QueryMap Map<String,String> params);
+    @POST("/api/profile/updatePhone")
+    Call<HashMap<String, String>> updatePhone(@Header("Authorization") String token, @Body Map<String, String> params);
 
     @GET("/med-inventory/search/nearbyShops")
-    Call<List<NearbyShopResponse>> findNearbyShops(@Header("Authorization") String token,@QueryMap Map<String,String> params);
+    Call<List<NearbyShopResponse>> findNearbyShops(@Header("Authorization") String token, @QueryMap Map<String, String> params);
 
     @POST("/med-inventory/search/shopshavingproducts")
-    Call<List<NearbyShopResponse>> getShopsHavingProducts(@Header("Authorization") String token,@Body HashMap<String,String> params);
+    Call<List<NearbyShopResponse>> getShopsHavingProducts(@Header("Authorization") String token, @Body HashMap<String, String> params);
 
     @GET("/med-inventory/search/category")
-    Call<List<ProductCatalogue>> getProductsOfCategory(@Header("Authorization") String token, @QueryMap Map<String,String> params);
+    Call<List<ProductCatalogue>> getProductsOfCategory(@Header("Authorization") String token, @QueryMap Map<String, String> params);
 
     @POST("/med-inventory/search/withincategory")
-    Call<List<ProductCatalogue>> getProductsWithinCategory(@Header("Authorization") String token,@Body HashMap<String,String> params);
+    Call<List<ProductCatalogue>> getProductsWithinCategory(@Header("Authorization") String token, @Body HashMap<String, String> params);
 
     @POST("/api/signup/invalid/delete")
-    Call<HashMap<String,String>> DeleteUser(@Body HashMap<String,String> params);
+    Call<HashMap<String, String>> DeleteUser(@Body HashMap<String, String> params);
+
+    @GET("/search/products_in_shop")
+    Call<List<HashMap<String, String>>> findShopProducts(@Header("Authorization") String token, @QueryMap HashMap<String, String> params);
+
+    @POST("/api/profile/updatePassword")
+    Call<HashMap<String, String>> updateProfilePassword(@Header("Authorization") String token, @Body Map<String, String> params);
+
 }
